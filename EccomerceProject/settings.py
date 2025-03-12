f741d03e34cf230d4e2654e2b4c13bf3bd97a47a
Mohammed Shaheem <mohammedshaheemtk2@gmail.com>
2025-03-12 19:52:24 +0530

    'emailfixed'

--- EccomerceProject/settings.py
+++ EccomerceProject/settings.py
@@ -65,8 +65,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = ('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = ('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #Google authentication
